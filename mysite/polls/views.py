d37688924342db34897e7f612a3bb309e5dc11e7
Anderson Lopez <andersonlopez.dev@gmail.com>
2022-08-01 16:41:41 -0500

    first commit

---
+++ mysite/polls/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import get_object_or_404, render, Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.template import loader
+from django.views import generic
+
+from .models import Choice,  Question
+# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    ... # same as above, no changes needed.
+####################
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+###############################    
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.  *Anderson Lopez*")
+    
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
